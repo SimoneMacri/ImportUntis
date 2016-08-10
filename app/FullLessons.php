533385f9cc2a6e147b48bae139ff4fb850af92c2
simonemacri <simone.macri>
2016-08-10 11:51:04 +0200

    a

--- app/FullLessons.php
+++ app/FullLessons.php
@@ -35,5 +35,5 @@ class FullLessons extends Model
 {
     protected $table = 'full_lessons';
     public $timestamps = false;
-    public $casts = ['teacher_id' => 'string'];
+    public $casts = ['teacher_id' => 'string', 'classe_id' => 'string'];
 }
